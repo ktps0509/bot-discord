3c28bcb3656e4c5106fc812a7fa4f9a06561f471
Vue <ktpskittipich@gmail.com>
2022-12-15 11:27:42 +0700

    condition join and leave

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import discord
 
 from discord.utils import get
 from discord.ext import commands
+from discord import app_commands
 from dotenv import load_dotenv
 from gtts import gTTS
 from discord import FFmpegPCMAudio
@@ -11,67 +12,53 @@ load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = commands.Bot(command_prefix='<', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-
-@client.event
+@bot.event
 async def on_ready():
-    print(f"Logged in As {client.user}")
+    print(f"Logged in As {bot.user}")
 
-
-# @client.event
-# async def on_message(message):
-#     # if the message is from the bot itself, ignore it
-#     if message.author == client.user:
-#         return
-
-#     if message.content.startswith('!join'):
-#         # the message is a command to join a voice channel, so we get the voice channel
-#         # that the user who sent the message is in
-#         user_voice_channel = message.author.voice.channel
-
-#         # if the user is not in a voice channel, we can't join it
-#         if user_voice_channel is None:
-#             await message.channel.send('You are not in a voice channel!')
-#             return
-
-#         # if the bot is already in a voice channel, we leave it first
-#         if client.voice_clients:
-#             await client.voice_clients[0].disconnect()
-
-#         # now we can join the user's voice channel
-#         await user_voice_channel.connect()
-
-
-@client.event
+@bot.event
 async def on_voice_state_update(member, before, after):
-    tts = gTTS(member.display_name, lang='th', slow=True)
-    tts.save('member.mp3')
+    if member.bot:
+        return
+    
+    # Joined
+    if before.channel is None and after.channel is not None:
+        tts = gTTS(member.display_name + ' เข้ามา', lang='th', slow=True)
+        tts.save('member.mp3')
 
-    guild = after.channel.guild
-    voice_client: discord.VoiceClient = discord.utils.get(
-        client.voice_clients, guild=guild)
-    audio_source = discord.FFmpegPCMAudio('member.mp3')
-    if not voice_client.is_playing():
-        voice_client.play(audio_source, after=None)
+        guild = after.channel.guild
+        voice_client: discord.VoiceClient = discord.utils.get(
+            bot.voice_clients, guild=guild)
+        audio_source = discord.FFmpegPCMAudio('member.mp3')
+        if not voice_client.is_playing():
+            voice_client.play(audio_source, after=None)
 
-    # voice_client: discord.VoiceClient = discord.utils.get(
-    #     client.voice_clients, guild=guild)
-    # audio_source = discord.FFmpegPCMAudio('member.mp3')
-    # if not voice_client.is_playing():
-    #     voice_client.play(audio_source, after=None)
+    # Leaved
+    if before.channel is not None and after.channel is None:
+        tts = gTTS(member.display_name + ' ออกไป', lang='th', slow=True)
+        tts.save('member.mp3')
 
+        guild = before.channel.guild
+        voice_client: discord.VoiceClient = discord.utils.get(
+            bot.voice_clients, guild=guild)
+        audio_source = discord.FFmpegPCMAudio('member.mp3')
+        if not voice_client.is_playing():
+            voice_client.play(audio_source, after=None)
+    
 
-@client.command()
+@bot.command()
 async def join(ctx):
     if (ctx.author.voice):
         channel = ctx.message.author.voice.channel
         await channel.connect()
+        await ctx.send("CommuBot joined")
     else:
         await ctx.send("You are not in a voice channel")
 
 
-@client.command()
+@bot.command()
 async def leave(ctx):
     if (ctx.voice_client):
         await ctx.guild.voice_client.disconnect()
@@ -80,18 +67,4 @@ async def leave(ctx):
         await ctx.send("CommuBot not in a voice channel")
 
 
-@client.command()
-async def play(ctx):
-    guild = ctx.guild
-    voice_client: discord.VoiceClient = discord.utils.get(
-        client.voice_clients, guild=guild)
-    audio_source = discord.FFmpegPCMAudio('member.mp3')
-    if not voice_client.is_playing():
-        voice_client.play(audio_source, after=None)
-
-
-@client.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-client.run(os.getenv('TOKEN'))
+bot.run(os.getenv('TOKEN'))
